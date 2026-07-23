e777cde03729a3126904c8d2e77c52f0945fabab
swombat <daniel.github@tenner.org>
2026-07-23 16:29:33 +0200

    Update navigation integration coverage

--- app/frontend/lib/components/navigation/navbar.svelte
+++ app/frontend/lib/components/navigation/navbar.svelte
@@ -20,6 +20,7 @@
   const currentUser = $derived($page.props?.user);
   const currentAccount = $derived($page.props?.account);
   const accounts = $derived($page.props?.accounts || []);
+  const accountHasWhiteboards = $derived($page.props?.account_has_whiteboards || false);
   const siteSettings = $derived($page.props?.site_settings);
 
   const links = $derived([
@@ -167,6 +168,7 @@
         {currentUser}
         {currentAccount}
         {accounts}
+        hasWhiteboards={accountHasWhiteboards}
         {currentTheme}
         onThemeChange={updateTheme}
         onLogout={handleLogout} />
